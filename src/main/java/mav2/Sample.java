550c604c496bd890f730054b1ba976994cd35232
Gouthi <gowtham.attili@nexturn.com>
2023-06-02 14:18:45 +0530

    Initial commit

---
+++ src/main/java/mav2/Sample.java
@@ -0,0 +1,9 @@
+package mav2;
+
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println("Hello Broooo");// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/mav2/SampleTest.java
@@ -0,0 +1,24 @@
+package mav2;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertEquals("Hello Broooo","Hello Broooo");
+	}
+
+}
